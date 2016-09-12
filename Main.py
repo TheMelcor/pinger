4873b7898e62a5302f9b8ea435942daeeca09838
Håkon Knudsen <hkn003@uib.no>
2016-09-12 19:56:23 +0200

    now colors the whole row while pinging

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ class PingerGui(Tkinter.Tk):
         self.p = Pinger()
         self.host_names = self.p.getHostNames()
         self.host_ips = self.p.getHostIp()
-        self.stime = 6000
+        self.stime = 60000
 
         self.labelsN = []
         self.labelsI = []
@@ -24,16 +24,17 @@ class PingerGui(Tkinter.Tk):
         x = 0
         for i in self.host_names:
             self.labelsN.append(Tkinter.Label(self, anchor="w", fg="black", text=i))
-            self.labelsN[x].grid(column=0, row=x, columnspan=2, sticky="ew")
+            self.labelsN[x].grid(column=0, row=x, columnspan=2, sticky="nsew")
             x += 1
 
         x = 0
         for i in self.host_ips:
             self.labelsI.append(Tkinter.Label(self, anchor="w", fg="black", bg="white", text=i))
-            self.labelsI[x].grid(column=2, row=x, columnspan=2, sticky="ew")
+            self.labelsI[x].grid(column=2, row=x, columnspan=2, sticky="nsew")
             x += 1
 
         self.grid_columnconfigure(0,weight=1)
+        self.grid_columnconfigure(2,weight=3)
         self.start()
 
     def setRed(self, target):
@@ -49,11 +50,14 @@ class PingerGui(Tkinter.Tk):
         x=0
         for i in self.host_ips:
             self.setYellow(self.labelsI[x])
+            self.setYellow(self.labelsN[x])
             self.update()
             if(self.p.checkSingleIp(x)):
                 self.setGreen(self.labelsI[x])
+                self.setGreen(self.labelsN[x])
             else:
                 self.setRed(self.labelsI[x])
+                self.setRed(self.labelsN[x])
             x+=1
         self.after(self.stime, self.start)
 
